6388f48c35fab094abffa787e865a301226cad2b
Zijie Zhao <32289372+zijzhao1996@users.noreply.github.com>
2023-12-27 15:02:03 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import os
+import multiprocessing
+from tqdm import tqdm
+
+def calculate_forward_return(args):
+    file_path, output_path = args
+    try:
+        # Read the CSV file
+        df = pd.read_csv(file_path)
+
+        # Ensure the dataframe is sorted by ticker and bar_time
+        df.sort_values(by=['ticker', 'bar_time'], inplace=True)
+
+        # Calculate forward return for each row
+        df['target'] = df.groupby('ticker')['close'].transform(
+            lambda x: x.shift(-6) / x - 1)
+
+        # Determine the output file path
+        output_file = os.path.join(output_path, os.path.basename(file_path))
+
+        # Save the modified dataframe
+        df.to_csv(output_file, index=False)
+    except Exception as e:
+        print(f"Error processing {file_path}: {e}")
+
+def main():
+    input_path = '/dat/chbr_group/chbr_scratch/test_mkt_data/volumes/'
+    output_path = '/dat/chbr_group/chbr_scratch/datadump_zijzhao/'
+
+    # Create output directory if it doesn't exist
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    # List all CSV files in the input directory
+    files = [(os.path.join(input_path, f), output_path) for f in os.listdir(input_path) if f.endswith('.csv')]
+
+    # Use multiprocessing to process files
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    for _ in tqdm(pool.imap_unordered(calculate_forward_return, files), total=len(files)):
+        pass
+
+    pool.close()
+    pool.join()
+
+if __name__ == "__main__":
+    main()
