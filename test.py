08d82f732480cd0d1d7108257e2c79ba3e3873ff
Zijie Zhao <32289372+zijzhao1996@users.noreply.github.com>
2023-12-27 17:04:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,53 +4,41 @@ import multiprocessing
 from tqdm import tqdm
 import time
 
-def calculate_forward_return(args):
-    file_path, output_path = args
+def process_file(file_name, market_data_path, volume_data_path, output_path):
     try:
-        # Read the CSV file
-        df = pd.read_csv(file_path)
+        # Read market and volume data CSV files
+        market_df = pd.read_csv(os.path.join(market_data_path, file_name))
+        volume_df = pd.read_csv(os.path.join(volume_data_path, file_name))
 
-        # Ensure the dataframe is sorted by ticker and bar_time
-        df.sort_values(by=['ticker', 'bar_time'], inplace=True)
+        # Merge dataframes on 'date', 'bar_time', 'ticker'
+        merged_df = pd.merge(market_df, volume_df, on=['date', 'bar_time', 'ticker'])
 
-        # Shift 'ret' by one to start from the next interval
-        df['shifted_ret'] = df.groupby('ticker')['ret'].shift(-1)
-
-        # Fill NA values with 0
-        df['shifted_ret'].fillna(0, inplace=True)
-
-        # Adjust 'shifted_ret' and compute the forward return
-        df['adj_ret'] = df['shifted_ret'] + 1
-        df['target'] = df.groupby('ticker')['adj_ret'].transform(
-            lambda x: x.rolling(6, min_periods=1).apply(lambda y: y.prod(), raw=True).shift(-6)) - 1
-
-        # Remove the temporary columns
-        df.drop(columns=['shifted_ret', 'adj_ret'], inplace=True)
-
-        # Determine the output file path
-        output_file = os.path.join(output_path, os.path.basename(file_path))
-
-        # Save the modified dataframe
-        df.to_csv(output_file, index=False)
+        # Save the merged dataframe as a parquet file
+        merged_df.to_parquet(os.path.join(output_path, file_name.replace('.csv', '.parquet')))
     except Exception as e:
-        print(f"Error processing {file_path}: {e}")
+        print(f"Error processing {file_name}: {e}")
 
 def main():
     start_time = time.time()  # Start timing
 
-    input_path = '/dat/chbr_group/chbr_scratch/test_mkt_data/volumes/'
-    output_path = '/dat/chbr_group/chbr_scratch/datadump_zijzhao/'
+    market_data_path = '/dat/chbr_group/chbr_scratch/test_mkt_data/mktdata/'
+    volume_data_path = '/dat/chbr_group/chbr_scratch/test_mkt_data/volumes/'
+    output_path = '/dat/chbr_group/chbr_scratch/datadump_merged/'
 
     # Create output directory if it doesn't exist
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    # List all CSV files in the input directory
-    files = [(os.path.join(input_path, f), output_path) for f in os.listdir(input_path) if f.endswith('.csv')]
+    # List all CSV files in the market data directory
+    market_files = set(os.listdir(market_data_path))
+    volume_files = set(os.listdir(volume_data_path))
+    
+    # Process only the files that are present in both directories
+    common_files = market_files.intersection(volume_files)
 
     # Use multiprocessing to process files
     pool = multiprocessing.Pool(multiprocessing.cpu_count())
-    for _ in tqdm(pool.imap_unordered(calculate_forward_return, files), total=len(files)):
+    for _ in tqdm(pool.imap_unordered(lambda f: process_file(f, market_data_path, volume_data_path, output_path), common_files), total=len(common_files)):
         pass
 
     pool.close()
@@ -61,3 +49,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
