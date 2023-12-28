7dd2b2dd98b4bca2d162074c73cad274990d5b45
Zijie Zhao <32289372+zijzhao1996@users.noreply.github.com>
2023-12-28 14:30:58 -0500

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -33,3 +33,51 @@ def obtain_features(path):
 path = '/dat/chbr_group/chbr_scratch/datadump_zijzhao/'
 features_df = obtain_features(path)
 
+
+
+
+
+
+
+
+
+import pandas as pd
+import os
+import concurrent.futures
+
+def process_file(file_path):
+    df = pd.read_parquet(file_path)
+
+    # Ensure the dataframe is sorted by ticker and bar_time
+    df.sort_values(by=['ticker', 'bar_time'], inplace=True)
+
+    # Define time intervals in terms of 5-minute bars
+    intervals = {'5mins': 1, '15mins': 3, '30mins': 6, '1hour': 12, '3hours': 36,
+                 '5hours': 60, '1day': 12*13, '3days': 12*13*3, '5days': 12*13*5, '10days': 12*13*10}
+
+    # Compute historical returns for each interval
+    for interval_name, interval_bars in intervals.items():
+        df[f'hist_ret_{interval_name}'] = df.groupby('ticker')['ret'].apply(
+            lambda x: x.iloc[::-1].rolling(window=interval_bars, min_periods=1).apply(
+                lambda y: (y + 1).prod() - 1, raw=True).iloc[::-1])
+
+    print(f"Processed {os.path.basename(file_path)}")
+    return df
+
+def obtain_features(path):
+    YEARS = ['2018', '2019', '2020']
+    files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.parquet') and any(f.startswith(year) for year in YEARS)]
+
+    # Process files in parallel using multiprocessing
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = executor.map(process_file, files)
+
+    # Concatenate all dataframes
+    combined_df = pd.concat(results, ignore_index=True)
+
+    return combined_df
+
+# Example usage
+path = '/dat/chbr_group/chbr_scratch/datadump_zijzhao/'
+features_df = obtain_features(path)
+
