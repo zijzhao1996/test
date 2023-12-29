b8df4f8d49bf4257de70194b880c3451b052af6e
Zijie Zhao <32289372+zijzhao1996@users.noreply.github.com>
2023-12-29 15:05:53 -0500

    Update process.py

--- process.py
+++ process.py
@@ -131,3 +131,87 @@ def load_and_preprocess(years, scale=10000, seq_len=20, temp_dir="temp_data"):
 
 # Rest of the code remains the same
 
+
+
+
+
+
+
+
+
+
+
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import multiprocessing as mp
+from functools import partial
+import os
+import pickle
+
+def process_ticker_data(df, ticker, scale, seq_len, year_temp_dir):
+    ticker_df = df[df['ticker'] == ticker]
+    nb_obs = len(ticker_df)
+    features = ticker_df[[col for col in ticker_df.columns if col.startswith('hist_return')]] * scale
+    target = ticker_df['target'] * scale
+
+    ticker_features = []
+    ticker_targets = []
+
+    for i in range(nb_obs - seq_len + 1):
+        X_seq = torch.FloatTensor(features.iloc[i:i + seq_len].values)
+        y_seq = torch.FloatTensor([target.iloc[i + seq_len - 1]])
+        ticker_features.append(X_seq)
+        ticker_targets.append(y_seq)
+
+    # Save processed data to disk
+    file_path = os.path.join(year_temp_dir, f"{ticker}.pkl")
+    with open(file_path, 'wb') as f:
+        pickle.dump((ticker_features, ticker_targets), f)
+
+    print(f"Processed ticker {ticker}: {len(ticker_features)} sequences.")
+
+def load_and_preprocess(years, scale=10000, seq_len=20, temp_dir="temp_data"):
+    all_features = []
+    all_targets = []
+
+    for year in years:
+        year_temp_dir = os.path.join(temp_dir, year)
+        if not os.path.exists(year_temp_dir):
+            os.makedirs(year_temp_dir)
+
+        file_path = f'{year}_data.parquet'
+        df = pd.read_parquet(file_path)
+        tickers = df['ticker'].unique()
+
+        with mp.Pool(mp.cpu_count()) as pool:
+            func = partial(process_ticker_data, df, scale=scale, seq_len=seq_len, year_temp_dir=year_temp_dir)
+            pool.map(func, tickers)
+
+        # Read processed data from disk
+        for ticker in tickers:
+            file_path = os.path.join(year_temp_dir, f"{ticker}.pkl")
+            with open(file_path, 'rb') as f:
+                ticker_features, ticker_targets = pickle.load(f)
+                all_features.extend(ticker_features)
+                all_targets.extend(ticker_targets)
+            os.remove(file_path)  # Optionally remove the file after loading
+
+    features_tensor = torch.stack(all_features)
+    targets_tensor = torch.stack(all_targets).squeeze(1)
+
+    return features_tensor, targets_tensor
+
+def create_dataloader(features, targets, batch_size=32, shuffle=True):
+    dataset = TensorDataset(features, targets)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+
+# Usage example
+# TRAIN_YEARS = ['2008', '2009']
+# VALID_YEARS = ['2010']
+# train_features, train_targets = load_and_preprocess(TRAIN_YEARS)
+# val_features, val_targets = load_and_preprocess(VALID_YEARS)
+# train_loader = create_dataloader(train_features, train_targets)
+# val_loader = create_dataloader(val_features, val_targets)
+
+
