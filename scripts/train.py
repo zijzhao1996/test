88956ee28ee0aac71a6491f57c8f74f99aa0761a
zijzhao1996 <zijiezha@mit.edu>
2024-01-04 14:12:25 -0500

    update

--- scripts/train.py
+++ scripts/train.py
@@ -1,15 +1,22 @@
 import os
 import pandas as pd
 from src.trainer import Trainer
+from src.data.dataloader import create_dataloader
 
 def main():
+    # Load configuration
     config_path = os.path.join(os.getcwd(), 'config', 'linear_model.yaml')
     trainer = Trainer(config_path)
 
     # Load data
-    train_dataloader = pd.read_parquet('/dat/chbr_group/chbr_scratch/non_sequential_data/2008_data.parquet')
-    valid_dataloader = pd.read_parquet('/dat/chbr_group/chbr_scratch/non_sequential_data/2009_data.parquet')
+    train_df = pd.read_parquet('/dat/chbr_group/chbr_scratch/non_sequential_data/2008_data.parquet')
+    valid_df = pd.read_parquet('/dat/chbr_group/chbr_scratch/non_sequential_data/2009_data.parquet')
 
+    # Create DataLoaders
+    train_dataloader = create_dataloader(train_df, batch_size=trainer.config['training_params']['batch_size'], shuffle=True)
+    valid_dataloader = create_dataloader(valid_df, batch_size=trainer.config['training_params']['batch_size'], shuffle=False)
+
+    # Train the model
     trainer.train(train_dataloader, valid_dataloader)
 
 if __name__ == "__main__":
