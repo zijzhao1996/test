77e867057a2634c0fd763c1e24c00d167bab8bcb
zijzhao1996 <zijiezha@mit.edu>
2024-01-09 14:30:11 -0500

    update code

--- scripts/train.py
+++ scripts/train.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 from trfm.trainer import Trainer
 from trfm.data.dataloader import create_dataloader
 from trfm.data.utils import determine_years
@@ -6,40 +7,43 @@ from trfm.data.utils import determine_years
 def main(config_path):
     # Load configuration
     trainer = Trainer(config_path)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - [%(levelname)s] - %(message)s')
 
     # Extract test year from the configuration
-    test_year = int(trainer.config['data_params']['test_year'])
+    test_years = trainer.config['data_params']['test_year']
+    assert isinstance(test_years, list), 'Years must be a list'
     is_seq = trainer.config['data_params']['is_seq']
+    seq_len = trainer.config['data_params']['seq_len'] if is_seq else None
 
     # Determine training and validation years
-    train_years, valid_years = determine_years(test_year)
+    for test_year in test_years:
+        train_years, valid_years = determine_years(test_year)
+        logging.info(f"Satrt to test on {test_year}...")
+        # Create DataLoaders for training, validation, and testing
+        train_dataloader = create_dataloader(years=train_years,
+                                            batch_size=trainer.config['training_params']['batch_size'],
+                                            shuffle=True,
+                                            scale=1e4,
+                                            seq_len=seq_len,
+                                            downsample=True,
+                                            is_seq=is_seq)
+        valid_dataloader = create_dataloader(years=valid_years,
+                                            batch_size=trainer.config['training_params']['batch_size'],
+                                            shuffle=False,
+                                            scale=1e4,
+                                            seq_len=seq_len,
+                                            downsample=True,
+                                            is_seq=is_seq)
+        test_dataloader = create_dataloader(years=[str(test_year)],
+                                            batch_size=trainer.config['training_params']['batch_size'],
+                                            shuffle=False,
+                                            scale=1e4,
+                                            seq_len=seq_len,
+                                            downsample=True,
+                                            is_seq=is_seq)
 
-    # Create DataLoaders for training, validation, and testing
-    seq_len = trainer.config['data_params']['seq_len'] if is_seq else None
-    train_dataloader = create_dataloader(years=train_years,
-                                        batch_size=trainer.config['training_params']['batch_size'],
-                                        shuffle=True,
-                                        scale=1e4,
-                                        seq_len=seq_len,
-                                        downsample=True,
-                                        is_seq=is_seq)
-    valid_dataloader = create_dataloader(years=valid_years,
-                                        batch_size=trainer.config['training_params']['batch_size'],
-                                        shuffle=False,
-                                        scale=1e4,
-                                        seq_len=seq_len,
-                                        downsample=True,
-                                        is_seq=is_seq)
-    test_dataloader = create_dataloader(years=[str(test_year)],
-                                        batch_size=trainer.config['training_params']['batch_size'],
-                                        shuffle=False,
-                                        scale=1e4,
-                                        seq_len=seq_len,
-                                        downsample=True,
-                                        is_seq=is_seq)
-
-    # Train the model
-    trainer.train(train_dataloader, valid_dataloader, test_dataloader, test_year)
+        # Train the model
+        trainer.train(train_dataloader, valid_dataloader, test_dataloader, test_year)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train model.')
--- trfm/data/utils.py
+++ trfm/data/utils.py
@@ -1,4 +1,5 @@
 def determine_years(test_year):
+    test_year = int(test_year)
     """Determine training and validation years based on the test year."""
     train_years = [str(test_year - 3), str(test_year - 2)]  # Last two years before the test year
     valid_years = [str(test_year - 1)]  # Year immediately before the test year
